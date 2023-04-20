13253d145758518af97b3baf00e1edc43a234af5
Ewelina <ewelinazawol@gmail.com>
2023-04-21 00:43:09 +0100

    Visual changes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,13 +7,13 @@ export default function Menu(): JSX.Element {
     <>
       <ul className="flex">
         <li className="flex-1 mr-2">
-          <Link
-            href="/"
+          <button
             className="group rounded-lg border border-transparent px-1 py-1 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            type="button"
+            onClick={() => router.push("/")}
           >
-            {" "}
-            HOME{" "}
-          </Link>
+            HOME
+          </button>
         </li>
         <li className="flex-1 mr-2">
           <button
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,20 +29,15 @@ export default function Home(): JSX.Element {
     },
   ];
   return (
-    <main className="flex min-h-screen flex-col items-center  p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          News
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+          <h1 className="mt-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
+            News
+          </h1>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By Ewelina - 2023
-          </a>
+        <div className="fixed bottom-4 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black  lg:h-auto  lg:bg-none">
+          By Ewelina - 2023
         </div>
       </div>
 
