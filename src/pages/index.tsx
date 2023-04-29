51cfd958553cfd17bc4bbb954c20b489fed31822
Ewelina <ewelinazawol@gmail.com>
2023-04-29 11:44:45 +0100

    Footer layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(): JSX.Element {
   interface LinksELement {
@@ -29,31 +25,35 @@ export default function Home(): JSX.Element {
     },
   ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div>
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
-          <h1 className="mt-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-            News
-          </h1>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <div>
+          <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+            <h1 className="mt-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
+              News
+            </h1>
+          </div>
         </div>
-        <div className="fixed bottom-4 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black  lg:h-auto  lg:bg-none">
+
+        {links.map((link) => {
+          return (
+            <Link key={link.path} href={`/news${link.path}`}>
+              <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-center">
+                <strong>{link.title}</strong>{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+                <p>{link.desc}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </main>
+      <footer className="m-4 dark:bg-gray-800 ">
+        <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center ">
           By Ewelina - 2023
         </div>
-      </div>
-
-      {links.map((link) => {
-        return (
-          <Link key={link.path} href={`/news${link.path}`}>
-            <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 text-center">
-              <strong>{link.title}</strong>{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-              <p>{link.desc}</p>
-            </div>
-          </Link>
-        );
-      })}
-    </main>
+      </footer>
+    </>
   );
 }
